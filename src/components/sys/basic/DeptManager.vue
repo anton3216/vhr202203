d98de7a82e9cb3f56c5ad39df7a6211a0eead512
Anton <anton3216@163.com>
2022-07-04 11:52:04 +0800

    vhr 完善权限组 完成部门管理 2022年7月4日11:51:59

--- src/components/sys/basic/DeptManager.vue
+++ src/components/sys/basic/DeptManager.vue
@@ -1,14 +1,100 @@
 <template>
-  <div>
-    部门管理
+  <div style="width : 500px">
+    <el-input size="mini" placeholder="输入关键字进行过滤" v-model="filterText">
+    </el-input>
+    <el-tree ref="tree" :data="depts" :props="defaultProps" node-key="id" :filter-node-method="filterNode"
+             :expand-on-click-node="false">
+      <span class="custom-tree-node" slot-scope="{ node, data }" style="width : 100%">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button size="mini" type="primary" @click="() => append(data)" class="treebutton">
+            添加部门
+          </el-button>
+          <el-button size="mini" type="danger" @click="() => remove(node, data)" class="treebutton">
+            删除部门
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+
+    <el-dialog title="添加部门" :visible.sync="dialogFormVisible" width="500px">
+      <el-tag type="warning">上级部门</el-tag>
+      <el-input style="margin-bottom: 10px;width: 180px" v-model="department.parentId" disabled
+                autocomplete="off"></el-input>
+      <br>
+      <el-tag type="warning">部门名称</el-tag>
+      <el-input style="width: 180px" placeholder="请输入部门名称..." v-model="department.name" autocomplete="off"></el-input>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="doInsert">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-  export default {
-    name : 'DeptManager'
+export default {
+  name: 'DeptManager',
+  data () {
+    return {
+      filterText: '',
+      depts: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      },
+      dialogFormVisible: false,
+      department: {
+        parentId: 0,
+        name: ''
+      }
+    }
+  },
+  methods: {
+    initDept () {
+      this.$http.getRequest('system/basic/dept').then(resp => {
+        if (resp) {
+          this.$data.depts = resp
+        }
+      })
+    },
+    filterNode (value, data) {
+      if (!value) return true
+      return data.name.indexOf(value) !== -1
+    },
+    append (data) {
+      this.$data.dialogFormVisible = true
+      this.$data.department.parentId = data.id
+    },
+    doInsert () {
+      this.$http.postRequest('system/basic/dept', this.$data.department).then(resp => {
+        if (resp) {
+          this.initDept()
+          this.$data.dialogFormVisible = false
+        }
+      })
+    },
+    remove (node, data) {
+      this.$http.deleteRequest('system/basic/dept/' + data.id).then(resp => {
+        if (resp) {
+          this.initDept()
+        }
+      })
+    }
+  },
+  mounted () {
+    this.initDept()
+  },
+  watch: {
+    filterText (val) {
+      this.$refs.tree.filter(val)
+    }
   }
+}
 </script>
 
 <style>
+.treebutton {
+  padding: 3px 7px;
+}
 </style>
--- src/components/sys/basic/PowerGroup.vue
+++ src/components/sys/basic/PowerGroup.vue
@@ -5,30 +5,25 @@
         <template slot="prepend">ROLE_</template>
       </el-input>
       <el-input size="small" class="addRole" placeholder="请输入角色中文名称" v-model="role.nameZh"></el-input>
-      <el-button size="small" type="primary">添加角色</el-button>
+      <el-button size="small" type="primary" @click="addRole">添加角色</el-button>
     </div>
-    <div>
+    <div style="margin-top: 10px; width: 600px;">
       <el-collapse accordion v-model="activeNames" @change="changeChecked">
         <el-collapse-item :title="item.nameZh" :key="index" v-for="(item,index) in roles" :name="item.id">
-          <div>
+          <div style="height:40px">
             可访问的资源
-            <el-button size="mini" circle icon="el-icon-delete" style="float: right;color: #f56c6c;"></el-button>
+            <el-button size="mini" circle icon="el-icon-delete" @click="deleteRole(item.id)"
+                       style="float: right;color: #f56c6c;"></el-button>
           </div>
           <div>
-            <el-tree ref="tree" :data="menus" show-checkbox node-key="id" :default-checked-keys="checkedIds"
-              :props="defaultProps">
-              <span class="custom-tree-node" slot-scope="{ node, data }">
-                <span>{{ node.label }}</span>
-                <span>
-                  <el-button type="text" style="font-weight:700;color:#e6a23c;" size="mini" @click="() => append(data)">
-                    追加
-                  </el-button>
-                  <el-button  type="text" style="font-weight:700;color:#f56c6c;" size="mini" @click="() => remove(node, data)">
-                    删除
-                  </el-button>
-                </span>
-              </span>
+            <el-tree default-expand-all ref="tree" :data="menus" show-checkbox node-key="id"
+                     :default-checked-keys="checkedIds"
+                     :props="defaultProps">
             </el-tree>
+            <div style="display: flex;justify-content: flex-end;">
+              <el-button size="mini" @click="cancelUpdate">取消修改</el-button>
+              <el-button size="mini" type="primary" @click="updateRole(item.id,index)">确认修改</el-button>
+            </div>
           </div>
         </el-collapse-item>
       </el-collapse>
@@ -37,74 +32,107 @@
 </template>
 
 <script>
-  export default {
-    name: 'PowerGroup',
-    data() {
-      return {
-        role: {
-          name: '',
-          nameZh: ''
-        },
-        activeNames: '',
-        roles: '',
-        menus: [],
-        defaultProps: {
-          children: 'children',
-          label: 'name'
-        },
-        checkedIds: []
-      }
-    },
-    methods: {
-      initRoles() {
-        this.$http.getRequest('system/config/role').then(resp => {
-          if (resp) {
-            this.roles = resp;
-          }
-        });
+export default {
+  name: 'PowerGroup',
+  data () {
+    return {
+      role: {
+        name: '',
+        nameZh: ''
       },
-      initMenus() {
-        this.$http.getRequest('system/basic/menus').then(resp => {
-          if (resp) {
-            this.menus = resp;
-          }
-        });
+      activeNames: '',
+      roles: '',
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name',
+        id: 'id'
       },
-      changeChecked() {
-        let rid = this.$data.activeNames;
-        this.initMenus();
-        if (rid != '') {
-          this.$http.getRequest('system/basic/menus/' + rid).then(resp => {
-            if (resp) {
-              let arr = [];
-              let returnarr = this.pushCheckedId(resp, arr);
-              this.$data.checkedIds = returnarr;
-            }
-          });
-        }
-      },
-      pushCheckedId(obj, arr) {
-        obj.forEach(o => {
-          if (o && o.id > 6) {
-            arr.push(o.id);
-          }
-          if (o.children && o.children instanceof Array) {
-            this.pushCheckedId(o.children, arr);
-          }
-        });
-        return arr;
-      }
-    },
-    mounted() {
-      this.initRoles();
-      this.initMenus();
+      checkedIds: []
     }
+  },
+  methods: {
+    initRoles () {
+      this.$http.getRequest('system/config/role').then(resp => {
+        if (resp) {
+          this.roles = resp
+        }
+      })
+    },
+    initMenus () {
+      this.$http.getRequest('system/basic/menus').then(resp => {
+        if (resp) {
+          this.menus = resp
+        }
+      })
+    },
+    changeChecked () {
+      this.$data.checkedIds = []
+      this.initMenus()
+      let rid = this.$data.activeNames
+      this.$http.getRequest('system/basic/menus/' + rid).then(resp => {
+        if (resp) {
+          let arr = []
+          this.$data.checkedIds = this.pushCheckedId(resp, arr)
+        }
+      })
+    },
+    pushCheckedId (obj, arr) {
+      obj.forEach(o => {
+        if (o && o.id > 6) {
+          arr.push(o.id)
+        }
+        if (o.children && o.children instanceof Array) {
+          this.pushCheckedId(o.children, arr)
+        }
+      })
+      return arr
+    },
+    addRole () {
+      this.$http.postRequest('system/config/role', this.$data.role).then(resp => {
+        if (resp) {
+          this.initRoles()
+          this.initMenus()
+        }
+      })
+    },
+    deleteRole (id) {
+      this.$http.deleteRequest('system/config/role/' + id).then(resp => {
+        if (resp) {
+          this.initRoles()
+          this.initMenus()
+        }
+      })
+    },
+    cancelUpdate () {
+      this.$data.activeNames = -1
+    },
+    updateRole (rid, index) {
+      let temp = this.$refs.tree[index].getCheckedKeys()
+      let mids = []
+      for (let i of temp) {
+        if (i > 6) {
+          mids.push(i)
+        }
+      }
+
+      this.$http.postRequest('/system/basic/menuRole/?rid=' + rid + '&mids=' + mids).then(res => {
+        if (res) {
+          this.$data.activeNames = -1
+        }
+      })
+    }
+  },
+  mounted () {
+    this.initRoles()
+    this.initMenus()
   }
+}
 </script>
 
 <style>
-  .addRole,
-  .input {
-    width: 250px;
-  }
+.addRole,
+.input {
+  width: 250px;
+}
 </style>
